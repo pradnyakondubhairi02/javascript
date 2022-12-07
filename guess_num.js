5aef5f887d6b5e8eb33f6520d5d341881b29fff7
pradnya kondubhairi <kondubhairipradnya@gmail.com>
2022-12-07 16:33:59 +0530

    first commit

---
+++ guess_num.js
@@ -0,0 +1,18 @@
+// let a=Math.random()*100;
+// a=Number.parseInt(a);
+// let guess;
+// let score=100;
+
+// while(guess!=a){
+//     score=score-1;
+//     guess=prompt("Enter the number:");
+//     if(guess==key)  {
+//         console.log("Congratulations!!! You guess the number");
+//       }
+//       else if(guess>key) {
+//         console.log("key number is smaller than your guess");
+//       }
+//       else{
+//         console.log("key number is bigger than your guess");
+//       }
+// }
---
+++ index.js
@@ -0,0 +1,42 @@
+// console.log("Hello World");
+// let name = "prady";
+// console.log(name);
+// // console.log(if);
+// // const intrst=1;
+// // intrst=2;
+// // console.log(intrst);
+
+// /* there are 2 categories of types
+//  1.primitive/values type - string,number,undefined,boolean,null
+//  2.referrance-object,arrays,function*/
+// //object---
+// console.log("heeyyy");
+//  let person ={
+//     name:'pradnya', 
+//     age:20
+//  };
+//  console.log(person);
+
+// //dot notation
+// person.name='engineer';
+// console.log(person.name);
+
+
+// //bracket
+// let selection ='name';
+// person[selection]='developer'
+// console.log(person.name);
+
+// //array---
+// let selector=['red','blue'];
+// console.log(selector);
+// console.log(selector[0]);
+// console.log(selector.entries);
+
+// //function
+// function greet(name){
+//    console.log("haahaa haa " + name);
+
+// }
+
+// greet('prady');
